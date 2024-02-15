a8762ac56e6ba482c0dc92a4f2f3e4a4e4c66f21
Obaidullah-ahsan <abir60303@gmail.com>
2024-02-16 02:50:59 +0600

    added final apps

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -1,12 +1,20 @@
 function playNow() {
-    addElementById('home-screen')
-    removeElementById('play-ground')
+    hideElementById('home-screen')
+    hideElementById('final-score')
+    showElementById('play-ground')
+    
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
     continueGame();
 }
-function score() {
-    addElementById('home-screen')
-    addElementById('play-ground')
-    removeElementById('final-score')
+function gameOver() {
+    hideElementById('home-screen')
+    hideElementById('play-ground')
+    showElementById('final-score')
+    const lastScore = getTextElementValueById('current-score');
+    setTextElementValueById('last-score', lastScore);
+    const currentAlphabet = getTextElementById('current-alphabet');
+    removeBacgroundColorById(currentAlphabet);
 }
 
 function continueGame() {
@@ -18,29 +26,52 @@ function continueGame() {
 }
 function handleButtonPrees(event) {
     const playerPressed = event.key;
-    // console.log('Player press', playerPressed);
+    if(playerPressed=== 'Escape'){
+        gameOver();
+    }
 
     const currentAlphabet = document.getElementById("current-alphabet");
     const alphabet = currentAlphabet.innerText;
     const alphabetLower = alphabet.toLowerCase();
     // console.log(playerPressed , alphabetLower);
-    if (alphabetLower === playerPressed){
-        console.log('You Got A Point');
+    if (playerPressed === alphabetLower) {
+        const currentScore = getTextElementValueById('current-score')
+        const score = currentScore + 1;
+        setTextElementValueById('current-score', score);
+        removeBacgroundColorById(alphabetLower);
+        continueGame();
     }
-    else{
-        console.log('You Missed , You Lost A Life');
+    else {
+        const currentLife = getTextElementValueById('current-life');
+        const life = currentLife - 1;
+        setTextElementValueById('current-life', life);
+        if (life === 0) {
+            gameOver()
+        }
+
     }
 }
 document.addEventListener('keyup', handleButtonPrees);
 
 // utility function
-function addElementById(elementId) {
+function hideElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('hidden')
 }
-function removeElementById(elementId) {
+function showElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
+}
+
+function getTextElementValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const valueText = element.innerText;
+    const value = parseInt(valueText);
+    return value;
+}
+function setTextElementValueById(elementId, value) {
+    const element = document.getElementById(elementId);
+    element.innerText = value;
 }
 
 function getARandomAlphabet() {
@@ -56,4 +87,14 @@ function getARandomAlphabet() {
 function setBacgroundColorById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('bg-[#FFA500]');
+}
+function removeBacgroundColorById(elementId) {
+    const element = document.getElementById(elementId)
+    element.classList.remove('bg-[#FFA500]');
+}
+
+function getTextElementById(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
 }
